0fc7a3c6690a988b940e7806da51c2758e36439b
Kutay Demireren <kutay.demireren@pinch.nl>
2024-04-27 20:55:44 +0200

    create application FlyMeTo

---
+++ FlyMeTo/FlyMeToApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyMeToApp.swift
+//  FlyMeTo
+//
+//  Created by Kutay Demireren on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlyMeToApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
